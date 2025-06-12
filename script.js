1557c99590e5768dce21712df66767aaf823440a
acandoo <117209328+acandoo@users.noreply.github.com>
2025-06-12 12:28:31 -0400

    refactor: break out clear function

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ const display = document.querySelector("#display");
 display.innerText += "9";
 let args = new Array(3).fill("");
 
+function clear() {
+    display.innerText = "";
+    args = new Array(3).fill("");
+}
+
 document.querySelectorAll(".numeric").forEach((button) => {
     button.addEventListener("click", () => {
         console.log(button.id);
@@ -15,7 +20,4 @@ document.querySelectorAll(".numeric").forEach((button) => {
 });
 
 document.querySelector("#clear")
-    .addEventListener("click", () => {
-        display.innerText = "";
-        args = new Array(3).fill("");
-    });
+    .addEventListener("click", clear);
