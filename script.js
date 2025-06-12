9e24dfebb8b80a55d7b9970fdf2cbebbe0db42e9
acandoo <117209328+acandoo@users.noreply.github.com>
2025-06-12 12:01:14 -0400

    feat: make numeric buttons interactive

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-import { add, subtract, multiply, divide } from "operations.js";
+import { add, divide, multiply, subtract } from "./operations.js";
+
+const numbers = Array(9).keys();
+const display = document.querySelector("#display");
+display.innerText += "9";
+let args = new Array(3).fill("");
+
+document.querySelectorAll(".numeric").forEach((button) => {
+    button.addEventListener("click", () => {
+        console.log(button.id);
+        display.innerText += button.id;
+        if (args[1] === "") {
+            
+        }
+    });
+});
+
+document.querySelector("#clear")
+    .addEventListener("click", () => {
+        display.innerText = "";
+        args = new Array(3).fill("");
+    });
