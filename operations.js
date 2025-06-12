cb28452d3add32ae3cf1fe8f5d8f5dc71d38714b
acandoo <117209328+acandoo@users.noreply.github.com>
2025-06-12 11:13:05 -0400

    feat: create operations JS module

---
+++ operations.js
@@ -0,0 +1,7 @@
+// Primitive math operations
+// i know this is insanely unnecessary but i like modules lol
+
+export let add = (a, b) => a + b;
+export let subtract = (a, b) => a - b;
+export let multiply = (a, b) => a * b;
+export let divide = (a, b) => a / b;
